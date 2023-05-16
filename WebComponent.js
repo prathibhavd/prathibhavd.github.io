f60380a8e2a71e4e083fbf1f16b228960b06a5ac
prathibhavd <132979187+prathibhavd@users.noreply.github.com>
2023-05-16 16:08:11 +0530

    WebComponent.js

--- WebComponent.js
+++ WebComponent.js
@@ -20,13 +20,31 @@
 
       connectedCallback() {
             console.log("In connectedCallback..."); 
-	    draw();
+	    linedraw(100,100,500,500);
         }
         
     }
+	
+    function linedraw(ax,ay,bx,by)
+	{
+    if(ay>by)
+    {
+        bx=ax+bx;  
+        ax=bx-ax;
+        bx=bx-ax;
+        by=ay+by;  
+        ay=by-ay;  
+        by=by-ay;
+    }
+    var calc=Math.atan((ay-by)/(bx-ax));
+    calc=calc*180/Math.PI;
+    var length=Math.sqrt((ax-bx)*(ax-bx)+(ay-by)*(ay-by));
+    document.body.innerHTML += "<div id='line' style='height:" + length + "px;width:1px;background-color:black;position:absolute;top:" + (ay) + "px;left:" + (ax) + "px;transform:rotate(" + calc + "deg);-ms-transform:rotate(" + calc + "deg);transform-origin:0% 0%;-moz-transform:rotate(" + calc + "deg);-moz-transform-origin:0% 0%;-webkit-transform:rotate(" + calc  + "deg);-webkit-transform-origin:0% 0%;-o-transform:rotate(" + calc + "deg);-o-transform-origin:0% 0%;'></div>"
+	}
     
     function draw() {   
-	    		var c = this._shadowRoot.getElementById("mycanvas");
+	    		/*
+			var c = this._shadowRoot.getElementById("mycanvas");
 			
 	    		var ctx = c.getContext("2d");
 	    		ctx.lineWidth = 3;
@@ -36,7 +54,7 @@
 			ctx.lineTo(300, 150);
 			ctx.stroke();
 	    		
-	    		/*
+	    		
 	    		console.log("In draw...");   
 			const canvas = this._shadowRoot.querySelector('sapCustomWidget');
 			
