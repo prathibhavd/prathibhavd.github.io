9da4f93a2bda1fc315fa730225f5aa255ab275b6
prathibhavd <132979187+prathibhavd@users.noreply.github.com>
2023-05-16 10:02:10 +0530

    WebComponent.js

--- WebComponent.js
+++ WebComponent.js
@@ -11,7 +11,7 @@
         }
 
         init() {            
-              
+            Console.log("In init..");  
             let shadowRoot = this.attachShadow({mode: "open"});
             shadowRoot.appendChild(tmpl.content.cloneNode(true));
                  
@@ -22,6 +22,8 @@
     }
     
     function draw() {
+	    
+	    		Console.log("In draw.."); 
 			const canvas = document.querySelector('#canvas');
 			
 			if(!canvas.getContext){
